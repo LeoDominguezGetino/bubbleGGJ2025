324954245febb82ed3eb80232f30155a3ff8db37
W E B <W-E-B-DEV@users.noreply.github.com>
2025-01-25 16:22:56 +0100

    Breakable Wall

---
+++ Assets/Scripts/Environment/BreakableWall.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class BreakableWall : MonoBehaviour
+{
+    [SerializeField] float minimumVelocityBreaking = 10;
+
+    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+            //GameManager.Instance.gameOver = true;
+        
+        if (collision.gameObject.layer == 7)
+        {
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            Debug.Log(rb.linearVelocity.magnitude + " && " + minimumVelocityBreaking);
+            if (rb.linearVelocity.magnitude >= minimumVelocityBreaking)
+            {
+                Destroy(this.gameObject);
+            }
+            
+        }
+    }
+}
