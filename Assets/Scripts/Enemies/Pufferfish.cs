23699c00eeabb77733442a4df55c5a7036aa8f65
W E B <W-E-B-DEV@users.noreply.github.com>
2025-01-25 13:57:07 +0100

    Pufferfish Script v2

--- Assets/Scripts/Enemies/Pufferfish.cs
+++ Assets/Scripts/Enemies/Pufferfish.cs
@@ -14,13 +14,20 @@ public class Pufferfish : MonoBehaviour
     private bool enableMovement;
     float currentDistance;
     int index;
-
+    public enum STATE
+    {
+        MOVE,
+        INFLATION
+    }
+    STATE currentState;
+    float inflatedDuration;
 
 
     private void Start()
     {
         if (positions.Count <= 1)
         {
+            currentState = STATE.INFLATION;
             enableMovement = false;
             if (positions.Count == 1)
             {
@@ -29,7 +36,7 @@ public class Pufferfish : MonoBehaviour
         }
         else
         {
-
+            currentState = STATE.MOVE;
             transform.position = positions[0];
             nextPosition = positions[1];
             index = 1;
@@ -43,6 +50,11 @@ public class Pufferfish : MonoBehaviour
         if (enableMovement)
         {
             moveToAPoint(nextPosition);
+            if(inflatedDuration > 0)
+            {
+                inflatedDuration -= Time.deltaTime;
+            }
+            if (inflatedDuration < 0) inflatedDuration = 0;
         }
 
     }
@@ -53,6 +65,39 @@ public class Pufferfish : MonoBehaviour
         currentDistance = Vector2.Distance(transform.position, destination);
         if (currentDistance == 0)
         {
+
+            switch (currentState)
+            {
+                
+
+                case STATE.MOVE:
+                    currentState = STATE.INFLATION;
+                    inflatedDuration = 2;
+                    break;
+
+                case STATE.INFLATION:
+
+                    if (inflatedDuration > 0)
+                    {
+                        Debug.Log("ESEGUI ANIMAZIONE");
+                        //ESEGUI CAMBIO SPRITES PUFFERFISH
+                    }
+                    else if (inflatedDuration == 0)
+                    {
+                        index++;
+                        if (index == positions.Count)
+                        {
+                            index = 0;
+                        }
+
+                        nextPosition = positions[index];
+                        currentState = STATE.MOVE;
+                    }
+                    break;
+
+            }
+
+            /*
             index++;
             if (index == positions.Count)
             {
@@ -60,7 +105,10 @@ public class Pufferfish : MonoBehaviour
             }
 
             nextPosition = positions[index];
+            */
 
         }
     }
+
+    
 }
