1f8f37fe78839db5530620f5051b96079ff727b2
leodominguez2003 <leodominguez2003@gmail.com>
2025-01-25 20:21:11 +0100

    Levers Logic

---
+++ Assets/Scripts/Environment/Lever.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Lever : MonoBehaviour
+{
+
+    [SerializeField] Transform handle;
+    [SerializeField] Transform outline;
+
+    [SerializeField] UnityEvent onActivated;
+
+    private void FixedUpdate()
+    {
+        if (Mathf.Abs(handle.localRotation.z)*100 > 45)
+        {
+            outline.gameObject.SetActive(true);
+            onActivated.Invoke();
+        }
+    }
+}
